f907bdf30ef0421a0951bb1534d6f9d194846e6b
ekza97 <ekza97@gmail.com>
2021-08-03 09:25:14 +0900

    Fix bug list_jabatan not working

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -3,6 +3,13 @@ function delMask( $str ) {
     return (int)implode('',explode('.',$str));
 }
 
+function list_jabatan(){
+    
+    $ci =& get_instance();
+    return $ci->db->order_by('golongan','DESC')
+                ->get('jabatan')->result_array();
+    
+}
 function count_table($table){
     
     $ci =& get_instance();
